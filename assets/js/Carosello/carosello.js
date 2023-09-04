852f98b7b7bf079573c75e95af68cd0202076576
Bonnolo <fbonetti00@gmail.com>
2023-09-04 17:40:05 +0200

    added video on home missing style

--- assets/js/Carosello/carosello.js
+++ assets/js/Carosello/carosello.js
@@ -5,27 +5,28 @@ const carosello = () => {
   const homeVideoButton1 = document.querySelector("#homeVideoButton1");
   const homeVideoButton2 = document.querySelector("#homeVideoButton2");
 
-  const video = ["1", "2", "3"];
+  const video = ["chemio_orizz.webm", "chirurgia_orizz.webm", "tc_orizz.webm"];
   let i = 0;
 
-  homeVideoButton1?.addEventListener("click", () => {
+  homeVideoButton1?.addEventListener("click", async () => {
+    await gsap.fromTo(homeVideo, { opacity: 1 }, { opacity: 0, duration: 0.5 });
     console.log("Avanti");
     if (i == 0) {
-      homeVideo?.setAttribute("src", `assets/video/video${video[2]}.mp4`);
+      homeVideo?.setAttribute("src", `assets/imgs/video/${video[2]}`);
       i = 2;
     } else {
-      homeVideo?.setAttribute("src", `assets/video/video${video[i - 1]}.mp4`);
+      homeVideo?.setAttribute("src", `assets/imgs/video/${video[i - 1]}`);
       i--;
     }
   });
 
-  homeVideoButton2?.addEventListener("click", () => {
+  homeVideoButton2?.addEventListener("click", async () => {
     console.log("Avanti");
     if (i == 2) {
-      homeVideo?.setAttribute("src", `assets/video/video${video[0]}.mp4`);
+      homeVideo?.setAttribute("src", `assets/imgs/video/${video[0]}`);
       i = 0;
     } else {
-      homeVideo?.setAttribute("src", `assets/video/video${video[i + 1]}.mp4`);
+      homeVideo?.setAttribute("src", `assets/imgs/video/${video[i + 1]}`);
       i++;
     }
   });
