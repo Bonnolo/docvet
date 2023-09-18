5b6000d3e719ff6a2caec74e4c22728bd9ad41aa
Bonnolo <fbonetti00@gmail.com>
2023-09-18 12:26:30 +0200

    added home video icons

--- assets/js/Carosello/carosello.js
+++ assets/js/Carosello/carosello.js
@@ -5,6 +5,10 @@ const carosello = () => {
   const homeVideoButton1 = document.querySelector("#homeVideoButton1");
   const homeVideoButton2 = document.querySelector("#homeVideoButton2");
 
+  const circleOne = document.querySelector("#circleOne");
+  const circleTwo = document.querySelector("#circleTwo");
+  const circleThree = document.querySelector("#circleThree");
+
   const video = ["chirurgia_orizz.webm", "chemio_orizz.webm", "tc_orizz.webm"];
   homeVideo?.setAttribute("src", `assets/imgs/video/${video[0]}`);
   let i = 0;
@@ -16,9 +20,11 @@ const carosello = () => {
     //  ease: "Power0.easeNone",
     //});
     //await gsap.to(homeVideo, { opacity: 0, duration: 0.1 });
-    console.log("Indietro");
+    //console.log("Indietro");
     if (i == 0) {
       homeVideo?.setAttribute("src", `assets/imgs/video/${video[2]}`);
+      circleOne?.classList.remove("circle-active");
+      circleThree?.classList.add("circle-active");
       // await gsap.fromTo(
       //   homeVideo,
       //   { x: -200 },
@@ -26,6 +32,13 @@ const carosello = () => {
       // );
       i = 2;
     } else {
+      if (i == 2) {
+        circleThree?.classList.remove("circle-active");
+        circleTwo?.classList.add("circle-active");
+      } else {
+        circleTwo?.classList.remove("circle-active");
+        circleOne?.classList.add("circle-active");
+      }
       homeVideo?.setAttribute("src", `assets/imgs/video/${video[i - 1]}`);
       // await gsap.fromTo(
       //   homeVideo,
@@ -37,11 +50,20 @@ const carosello = () => {
   });
 
   homeVideoButton2?.addEventListener("click", async () => {
-    console.log("Avanti");
+    //console.log("Avanti");
     if (i == 2) {
       homeVideo?.setAttribute("src", `assets/imgs/video/${video[0]}`);
+      circleOne?.classList.add("circle-active");
+      circleThree?.classList.remove("circle-active");
       i = 0;
     } else {
+      if (i == 0) {
+        circleOne?.classList.remove("circle-active");
+        circleTwo?.classList.add("circle-active");
+      } else {
+        circleTwo?.classList.remove("circle-active");
+        circleThree?.classList.add("circle-active");
+      }
       homeVideo?.setAttribute("src", `assets/imgs/video/${video[i + 1]}`);
       i++;
     }
